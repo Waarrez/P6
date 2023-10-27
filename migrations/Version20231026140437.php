097896f5fd4b1d5b4e8c469ccfb65948ac5ca3c0
Warez-amasya <thimote.cabotte6259@gmail.com>
2023-10-27 12:32:07 +0200

    add trick, edit and delete

---
+++ migrations/Version20231026140437.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231026140437 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `group` (id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE trick (id VARCHAR(255) NOT NULL, groups_id VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, images VARCHAR(255) NOT NULL, medias VARCHAR(255) DEFAULT NULL, INDEX IDX_D8F0A91EF373DCF (groups_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE trick ADD CONSTRAINT FK_D8F0A91EF373DCF FOREIGN KEY (groups_id) REFERENCES `group` (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE trick DROP FOREIGN KEY FK_D8F0A91EF373DCF');
+        $this->addSql('DROP TABLE `group`');
+        $this->addSql('DROP TABLE trick');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,135 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Form\TrickFormType;
+use App\Repository\TrickRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly TrickRepository $trickRepository
+    )
+    {}
+
     #[Route('/', name: 'home.index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+
+        $tricks = $this->trickRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricks
+        ]);
+    }
+
+    #[Route('/addTrick', name: 'home.addTrick')]
+    public function addTrick(Request $request) : Response {
+
+        $trick = new Trick();
+        $newFileName = null;
+
+        $form = $this->createForm(TrickFormType::class, $trick);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('imageFile')->getData();
+
+            if($file) {
+                $uploadDirectory = $this->getParameter("upload_directory");
+                $newFileName = uniqid().'.'.$file->guessExtension();
+
+                try {
+                    $file->move($uploadDirectory, $newFileName);
+                } catch (\Exception $e) {
+
+                }
+
+                $category = $form->get('groups')->getData();
+                $trick->setImages($newFileName);
+                $trick->setGroups($category);
+                $trick->setCreatedAt(new \DateTimeImmutable());
+
+                $this->entityManager->persist($trick);
+                $this->entityManager->flush();
+            }
+        }
+
+        return $this->render('tricks/add_trick.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/trick/detail/{id}', name: "home.viewTrick")]
+    public function viewTrick(string $id) : Response {
+
+        $trick = $this->trickRepository->find($id);
+
+        return $this->render('tricks/view_trick.html.twig', [
+            'trick' => $trick
+        ]);
+    }
+
+    #[Route('/trick/edit/{id}', name: "home.editTrick")]
+    public function editTrick(string $id, Request $request) : Response {
+
+        $trick = $this->trickRepository->find($id);
+
+        $form = $this->createForm(TrickFormType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            if($trick->getImages() !== null) {
+                $file = $form->get('imageFile')->getData();
+                if($file) {
+                    $uploadDirectory = $this->getParameter("upload_directory");
+                    $newFileName = uniqid().'.'.$file->guessExtension();
+                    try {
+                        $file->move($uploadDirectory, $newFileName);
+                    } catch (\Exception $e) {
+
+                    }
+                    $trick->setImages($newFileName);
+                }
+            }
+
+            $trick->setEditAt(new \DateTimeImmutable());
+            $this->entityManager->persist($trick);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('home.index');
+        }
+
+        return $this->render('tricks/edit_trick.html.twig', [
+            'form'  => $form
+        ]);
+
+    }
+
+    #[Route('/trick/remove/{id}', name: "home.deleteTrick")]
+    public function deleteTrick(string $id) : Response {
+
+        $trick = $this->trickRepository->find($id);
+
+        $imagePath = $this->getParameter('upload_directory') . '/' . $trick->getImages();
+
+        if (file_exists($imagePath)) {
+            // Supprimez l'image du dossier
+            unlink($imagePath);
+        }
+
+
+        $this->entityManager->remove($trick);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('home.index');
+
     }
 }
