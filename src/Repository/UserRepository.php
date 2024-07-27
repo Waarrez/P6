23481c6ec4763708dcbeb727f29a6f1d137e755d
Warez-amasya <thimote.cabotte6259@gmail.com>
2024-07-27 14:21:30 +0200

    fix code for symfonyInsigth

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -57,29 +57,4 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $plainPassword = $credentials->getPassword();
         return $this->passwordHasher->isPasswordValid($user, $plainPassword);
     }
-
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
